5e08ddf41cb73b9346d99133f6dd0be0a6e7176a
AleksandrFROLik <nextfrolik@gmail.com>
2021-10-24 14:00:50 +0300

    HW - 6 finish

--- src/p2-homeworks/h5/pages/Junior.tsx
+++ src/p2-homeworks/h5/pages/Junior.tsx
@@ -1,10 +1,11 @@
 import React from "react";
+import HW6 from "../../h6/HW6";
 
 
 export const Junior = () => {
     return (
         <div>
-            {/*<HW6/>*/}
+            <HW6/>
             {/*<HW7/>*/}
             {/*<HW8/>*/}
             {/*<HW9/>*/}
