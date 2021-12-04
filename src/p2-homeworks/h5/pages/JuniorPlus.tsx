c1f1d19f77296002103f0d13803b1a76479015da
AleksandrFROLik <nextfrolik@gmail.com>
2021-12-04 21:22:04 +0300

    HW 12 finished+

--- src/p2-homeworks/h5/pages/JuniorPlus.tsx
+++ src/p2-homeworks/h5/pages/JuniorPlus.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import style from "./JuniorPlus.module.css"
 import HW10 from "../../h10/HW10";
 import HW11 from "../../h11/HW11";
 import HW12 from "../../h12/HW12";
@@ -6,7 +7,7 @@ import HW12 from "../../h12/HW12";
 
 export const JuniorPlus = () => {
     return (
-        <div>
+        <div className={style.juniorPlusBlock}>
             <HW10/>
             <HW11/>
             <HW12/>
